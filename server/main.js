ad1d8a820a5a306ffbc8d2dfbab0003908b0cb6f
Dan Quellhorst <dan@quellhorst.com>
2016-08-11 05:00:01 -0500

    Temp disable creating index

--- server/main.js
+++ server/main.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  BitcoinPrices.rawCollection().createIndex({createdAt: 1}, function () {});
+  //BitcoinPrices.rawCollection().createIndex({createdAt: 1}, function () {});
 });
